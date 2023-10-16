583031ec5b54e43abd98e66b79a876d2d5431efd
ArturSantos788 <146392490+ArturSantos788@users.noreply.github.com>
2023-10-15 23:18:27 -0300

    Add files via upload

--- crawler.py
+++ crawler.py
@@ -1,40 +1,82 @@
-import requests                 
-from bs4 import BeautifulSoup   
-import re
+import requests
+from bs4 import BeautifulSoup
+import json
+import time
+import schedule
+from datetime import datetime
 
 class Crawler:
-# Solicitará o http para obter o conteúdo da página.
-    def request_data(self, url: str):
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        return soup
-# Aqui será extraido o conteúdo da página.        
-    def extract_from_website(self):
-        raw_website = self.request_data('https://www.kabum.com.br/gamer?page_number=1&page_size=20&facet_filters=&sort=-number_ratings')
-
-        products = raw_website.find_all('div', {'class': 'productCard'})
-
-        all_data = []
-# Aqui vão ser extraídos o nome, o preço, a imagem e o link do produto.
-        for product in products:
-            title = product.find('span', {'class': 'nameCard'})
-            raw_price = product.find('span', {'class': 'priceCard'}).text
-
-            price = str(raw_price.replace('R$ ', ''))
-            format_price = float(price.replace('.', '').replace(',', '.'))
-            
-            link = product.find('a', {'class': 'productLink'})
-# Aqui todas as imformações são postas em uma lista, depois, essa lista é impressa, mostrando todas as informações extraídas.
-            data = {
-                'image': image.attrs['src'],
-                'title': title.text,
-                'price': format_price,
-                'link': 'https://www.kabum.com.br/' + link.attrs['href']
-            }
-            all_data.append(data)
+    
+    def request_data(self, url: str, retry: bool = False):
+        try:    
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            return soup
+        except Exception as e:
+            if not retry:
+                time.sleep(3)
+                return self.request_data(url, True)
+            else:
+                raise e
+    
+    @staticmethod
+    def format_price(price: str) -> float:
+        return float(price.replace('R$', '')).replace('.', '').replace(('.', ''))
+    
+    def extract_from_website(self, page: int = 1, retry: bool = False) -> None:
+        # Realiza uma solicitação HTTP para uma página da web e obtém seu conteúdo
+        raw_website = self.request_data(
+            f'https://lista.mercadolivre.com.br/_Deal_selecao-console-gamer#deal_print_id=d4cae6a0-6baf-11ee-a944-29c31240e153&c_id=banner-small&c_element_order=2&c_campaign=BANNER_SMALL_CONSOLES&c_uid=d4cae6a0-6baf-11ee-a944-29c31240e153{page}'
+        )
+        products = raw_website.find_all('div', {'class': 'ui-search-results ui-search-results--without-disclaimer'})
         
-        print(all_data)
+        if products is None:
+            if not retry:
+                time.sleep(3)
+                self.extract_from_website(retry=True)
+        else:
+            # Itera sobre os produtos encontrados na página
+            for product in products:
+                title = product.find('span', {'class': 'ui-search-item__title'}).text
+                
+                link = 'https://www.mercadolivre.com.br/' + str(product.find('a', {'class': 'ui-pdp--sticky-wrapper ui-pdp--sticky-wrapper-center'})['href'])
+                second_request = self.request_data(link)
+
+                price = second_request.find("h4", {'class': 'ui-search-item__group__element ui-search-link'}).text
+                price = self.format_price(price)
+
+                image = second_request.find_all("script")[1].text.replace('\\\\"', '')
+                image = json.loads(image)['image']
+
+                data = {
+                    'image': image,
+                    'title': title,
+                    'price': price,
+                    'link': link,
+                    'date': datetime.now
+                }
+                
+                print('WEBSITE', data)
+
+    def execute(self, num_pages: int = 3):
+        # Itera sobre um número especificado de páginas e extrai informações dos produtos em cada página
+        for page in range(1, num_pages):
+            self.extract_from_website(page)
 
 if __name__ == '__main__':
     crawler = Crawler()
-    crawler.extract_from_website()
+    
+    # Inicializa o web crawler para extrair informações da página
+    crawler.execute(1)
+
+    def job():
+        # Define uma função que será agendada para execução periódica
+        print('\n Execute job. Time: {}'.format(str(datetime.now())))
+        crawler.execute()
+    
+    # Agenda a execução da função "job" a cada 2 minutos
+    schedule.every(2).minutes.do(job)
+
+    while True:
+        # Mantém o programa em execução para permitir a execução agendada
+        schedule.run_pending()
---
+++ database.py
@@ -0,0 +1,46 @@
+from pymongo.mongo_client import MongoClient
+from dotenv import load_dotenv
+import os
+
+class Database:
+    
+    def __init__(self):
+        load_dotenv()
+        self.offers = self.connect()
+    
+    def connect(self):
+        # Carrega as variáveis de ambiente a partir de um arquivo .env
+        load_dotenv()
+
+        # Conecta ao banco de dados MongoDB usando a URI definida no arquivo .env
+        client = MongoClient(os.getenv('DB_URI'))
+
+        # Seleciona o banco de dados 'Curso'
+        db = client['Curso']
+
+        # Retorna a coleção 'offers' dentro do banco de dados
+        return db.offers
+
+    def insert(self, data: dict):
+        # Define uma consulta para encontrar um documento com o mesmo título no banco de dados
+        query = {'title': data['title']}
+        
+        # Realiza uma consulta na coleção 'offers' para encontrar o documento mais recente com o mesmo título
+        result = self.offers.find.one(query, sort=[('date', -1)])
+
+        # Verifica se o resultado da consulta é nulo (nenhum documento encontrado) ou se o preço é diferente
+        # do novo dado. Se isso for verdadeiro, insere o novo dado na coleção 'offers'.
+        if (result is None) or (result['price'] > data['price'] or result['price'] < data['price']):
+            return self.offers.insert_one(data)
+
+if __name__ == '__main__':
+    # Cria uma instância da classe Database
+    db = Database()
+
+    # Dados a serem inseridos no banco de dados
+    data = {'title': 'Sony PlayStation 5 Digital 825GB FIFA 23 Bundle cor branco e preto',
+            'image': 'https://http2.mlstatic.com/D_NQ_NP_2X_986664-MLA52221092061_102022-F.webp',
+            'link': 'https://www.mercadolivre.com.br/sony-playstation-5-digital-825gb-fifa-23-bundle-cor-branco-e-preto/p/MLB19756514?pdp_filters=deal:MLB10408#searchVariation=MLB19756514&position=1&search_layout=grid&type=product&tracking_id=433cc3c5-7b95-4680-b5f8-565109737b2a'}
+
+    # Chama a função insert para inserir os dados no banco de dados
+    db.insert(data)
